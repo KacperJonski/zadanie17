046a109128fccd73437b12b75194b83a677ce8c9
Kacper Jonski <kacper.jonski@onet.pl>
2026-01-16 10:50:07 +0100

    Dodano menu wyboru brył

--- hello.c
+++ hello.c
@@ -3,32 +3,44 @@
 #define PI 3.14159265359
 
 int main() {
+    int wybor;
     float a, b, c;
     float r;
-    float objetosc_prostopadloscianu;
-    float objetosc_kuli;
+    float objetosc;
 
+    printf("===== MENU =====\n");
+    printf("1. Oblicz objetosc prostopadloscianu\n");
+    printf("2. Oblicz objetosc kuli\n");
+    printf("Wybierz opcje (1 lub 2): ");
+    scanf("%d", &wybor);
 
-    printf("Podaj dlugosc boku a prostopadloscianu: ");
-    scanf("%f", &a);
+    switch (wybor) {
+        case 1:
+            printf("\nPodaj dlugosc boku a: ");
+            scanf("%f", &a);
 
-    printf("Podaj dlugosc boku b prostopadloscianu: ");
-    scanf("%f", &b);
+            printf("Podaj dlugosc boku b: ");
+            scanf("%f", &b);
 
-    printf("Podaj dlugosc boku c prostopadloscianu: ");
-    scanf("%f", &c);
+            printf("Podaj dlugosc boku c: ");
+            scanf("%f", &c);
 
-    objetosc_prostopadloscianu = a * b * c;
-    printf("\nObjetosc prostopadloscianu: %.2f\n", objetosc_prostopadloscianu);
+            objetosc = a * b * c;
+            printf("\nObjetosc prostopadloscianu wynosi: %.2f\n", objetosc);
+            break;
 
-    printf("\nPodaj promien kuli: ");
-    scanf("%f", &r);
+        case 2:
+            printf("\nPodaj promien kuli: ");
+            scanf("%f", &r);
 
-    objetosc_kuli = (4.0 / 3.0) * PI * r * r * r;
+            objetosc = (4.0 / 3.0) * PI * r * r * r;
+            printf("\nObjetosc kuli wynosi: %.2f\n", objetosc);
+            break;
 
-
-
-    printf("Objetosc kuli: %.2f\n", objetosc_kuli);
+        default:
+            printf("\nBlad! Nieprawidlowy wybor.\n");
+    }
 
     return 0;
 }
+
