09ab34c5dcd37cf2e6f4c0ab9c5d27de26c68181
Kacper Jonski <kacper.jonski@onet.pl>
2026-01-16 10:42:29 +0100

    Dodano wzór na objętośc kuli

--- hello.c
+++ hello.c
@@ -1,21 +1,34 @@
 #include <stdio.h>
 
+#define PI 3.14159265359
+
 int main() {
     float a, b, c;
-    float objetosc;
+    float r;
+    float objetosc_prostopadloscianu;
+    float objetosc_kuli;
 
-    printf("Podaj dlugosc boku a: ");
+
+    printf("Podaj dlugosc boku a prostopadloscianu: ");
     scanf("%f", &a);
 
-    printf("Podaj dlugosc boku b: ");
+    printf("Podaj dlugosc boku b prostopadloscianu: ");
     scanf("%f", &b);
 
-    printf("Podaj dlugosc boku c: ");
+    printf("Podaj dlugosc boku c prostopadloscianu: ");
     scanf("%f", &c);
 
-    objetosc = a * b * c;
+    objetosc_prostopadloscianu = a * b * c;
+    printf("\nObjetosc prostopadloscianu: %.2f\n", objetosc_prostopadloscianu);
 
-    printf("Objetosc prostopadloscianu wynosi: %.2f\n", objetosc);
+    printf("\nPodaj promien kuli: ");
+    scanf("%f", &r);
+
+    objetosc_kuli = (4.0 / 3.0) * PI * r * r * r;
+
+
+
+    printf("Objetosc kuli: %.2f\n", objetosc_kuli);
 
     return 0;
 }
