1fe2d899e696aeef0dfeab39e67807c90cd3eee4
Kacper Jonski <kacper.jonski@onet.pl>
2026-01-16 10:55:06 +0100

    Dodano ostrosłup czworokątny prawidłowy

--- hello.c
+++ hello.c
@@ -6,16 +6,19 @@ int main() {
     int wybor;
     float a, b, c;
     float r;
+    float ap, h;
     float objetosc;
 
     printf("===== MENU =====\n");
     printf("1. Oblicz objetosc prostopadloscianu\n");
     printf("2. Oblicz objetosc kuli\n");
-    printf("Wybierz opcje (1 lub 2): ");
+    printf("3. Oblicz objetosc ostroslupa prawidlowego czworokatnego\n");
+    printf("Wybierz opcje (1, 2 lub 3): ");
     scanf("%d", &wybor);
 
     switch (wybor) {
         case 1:
+
             printf("\nPodaj dlugosc boku a: ");
             scanf("%f", &a);
 
@@ -26,15 +29,28 @@ int main() {
             scanf("%f", &c);
 
             objetosc = a * b * c;
-            printf("\nObjetosc prostopadloscianu wynosi: %.2f\n", objetosc);
+            printf("\nObjetosc prostopadloscianu: %.2f\n", objetosc);
             break;
 
         case 2:
+
             printf("\nPodaj promien kuli: ");
             scanf("%f", &r);
 
             objetosc = (4.0 / 3.0) * PI * r * r * r;
-            printf("\nObjetosc kuli wynosi: %.2f\n", objetosc);
+            printf("\nObjetosc kuli: %.2f\n", objetosc);
+            break;
+
+        case 3:
+
+            printf("\nPodaj dlugosc boku podstawy a: ");
+            scanf("%f", &ap);
+
+            printf("Podaj wysokosc ostroslupa h: ");
+            scanf("%f", &h);
+
+            objetosc = (1.0 / 3.0) * ap * ap * h;
+            printf("\nObjetosc ostroslupa prawidlowego czworokatnego: %.2f\n", objetosc);
             break;
 
         default:
